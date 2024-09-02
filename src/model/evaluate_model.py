09ec7715446f54c7c92e7e102da88ae991eeb044
MBenediktF <mbf@fassian.com>
2024-09-02 09:19:46 +0200

    refactor: removed print statement

--- src/model/evaluate_model.py
+++ src/model/evaluate_model.py
@@ -1,6 +1,5 @@
 def evaluate_model(model, x_test, y_test):
     # Modell evaluieren
     test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
-    print('\nTest accuracy:', test_acc)
 
     return test_loss, test_acc
