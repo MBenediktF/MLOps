ed5b3123a15532af1969a6687e5b768f5a7a9f7e
MBenediktF <mbf@fassian.com>
2024-09-03 10:04:44 +0200

    dev: added submodules

--- src/experiment_multiple_runs.py
+++ src/experiment_multiple_runs.py
@@ -1,75 +1,69 @@
-
 import os
 import tensorflow as tf
 import certifi
 import mlflow
 import mlflow.tensorflow
 
+from model.import_data import import_data
+from model.preprocess_data import preprocess_data
+from model.create_model import create_model
+from model.fit_model import fit_model
+from model.evaluate_model import evaluate_model
 
-def start_mlflow_run(lr, dr, epochs):
-    with mlflow.start_run():
-        # Log the hyperparameters
-        mlflow.log_param("learning_rate", lr)
-        mlflow.log_param("dropout_rate", dr)
-        mlflow.log_param("epochs", epochs)
+print("TensorFlow version:", tf.__version__)
 
-        # Build the model
-        model = tf.keras.models.Sequential([
-            tf.keras.layers.Flatten(input_shape=(28, 28)),
-            tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dropout(dr),
-            tf.keras.layers.Dense(10)
-        ])
-
-        # Compile the model
-        model.compile(
-            optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(
-                from_logits=True
-            ),
-            metrics=['accuracy']
-        )
-
-        # Train the model
-        model.fit(x_train, y_train, epochs=epochs, verbose=1)
-
-        # Evaluate the model
-        loss, accuracy = model.evaluate(x_test, y_test, verbose=2)
-
-        # Log the metrics
-        mlflow.log_metric("loss", loss)
-        mlflow.log_metric("accuracy", accuracy)
-
-        # Save the model as an artifact
-        model_path = "model.h5"
-        model.save(model_path)
-        mlflow.log_artifact(model_path)
-
-        mlflow.tensorflow.log_model(model, "model")
-
-        mlflow.end_run()
-
-
-# Set the SSL certificate
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
-# Load the MNIST dataset
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Define hyperparameter grid
-param_grid = {
-    'learning_rate': [0.001, 0.01],
-    'dropout_rate': [0.2, 0.3],
-    'epochs': [5, 10]
-}
+def mlflow_run(x_train, y_train, x_test, y_test, dropout, epochs):
+    with mlflow.start_run():
 
-# Start MLflow
-mlflow.set_experiment("MNIST Hyperparameter Tuning 2")
+        mlflow.tensorflow.autolog()
 
-# Iterate over all combinations of hyperparameters
-for lr in param_grid['learning_rate']:
-    for dr in param_grid['dropout_rate']:
-        for epochs in param_grid['epochs']:
-            start_mlflow_run(lr, dr, epochs)
+        # Log the hyperparameters
+        mlflow.log_param("dropout", dropout)
+        mlflow.log_param("epochs", epochs)
+
+        # Modell erstellen
+        model = create_model(dropout=0.2)
+
+        # Modell trainieren
+        fit_model(model, x_train, y_train,
+                  optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'],
+                  epochs=epochs)
+
+        # Modell evaluieren
+        eval = evaluate_model(model, x_test, y_test)
+
+        # Metriken loggen
+        mlflow.tensorflow.mlflow.log_metric("evaluation_accuracy", eval[1])
+
+    mlflow.end_run()
+
+
+def main():
+    # Daten importieren
+    x_train, y_train, x_test, y_test = import_data()
+
+    # Daten vorverarbeiten
+    x_train, x_test = preprocess_data(x_train, x_test)
+
+    # Define hyperparameter grid
+    parameters = {
+        'dropout': [0.2, 0.3],
+        'epochs': [5, 10]
+    }
+
+    # Set experiment
+    mlflow.set_experiment("MNIST Multiple Runs")
+
+    # Iterate over all combinations of hyperparameters
+    for dropout in parameters['dropout']:
+        for epochs in parameters['epochs']:
+            mlflow_run(x_train, y_train, x_test, y_test, dropout, epochs)
+
+
+if __name__ == "__main__":
+    main()
