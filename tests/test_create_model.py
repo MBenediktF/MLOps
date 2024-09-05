bbbc92196d79399558747cbf0e75c0997525fa17
MBenediktF <mbf@fassian.com>
2024-09-05 11:18:08 +0200

    dev: advenced testing

--- tests/test_create_model.py
+++ tests/test_create_model.py
@@ -1,6 +1,8 @@
 from src.model.create_model import create_model
+import tensorflow as tf
 
 
 def test_create_model():
     model = create_model(dropout=0.2)
-    assert model is not None
+    assert isinstance(model, tf.keras.Model), "The returned model is not a Keras model"
+    assert len(model.layers) > 0, "The returned model has no layers"
