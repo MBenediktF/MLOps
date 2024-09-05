f51130873d72f645ff01665280627bc1ea8c0a5c
MBenediktF <mbf@fassian.com>
2024-09-05 14:38:18 +0200

    dev: added parameters to tests

--- tests/test_create_model.py
+++ tests/test_create_model.py
@@ -3,14 +3,18 @@ import tensorflow as tf
 import pytest
 
 
-def test_create_model():
+@pytest.mark.parametrize("dropout", [0.2, 0.8])
+def test_create_model_valid(dropout):
     # check if the function returns a valid Keras model
     model = create_model(dropout=0.2)
-    assert isinstance(model, tf.keras.Model), "The returned model is not a Keras model"
-    assert len(model.layers) > 0, "The returned model has no layers"
+    assert isinstance(model, tf.keras.Model), \
+        "The returned model is not a Keras model"
+    assert len(model.layers) > 0, \
+        "The returned model has no layers"
 
+
+@pytest.mark.parametrize("dropout", [-0.5, 1.2])
+def test_create_model_invalid(dropout):
     # check how the funtion handles wrong dropout parameter
     with pytest.raises(ValueError):
-        model = create_model(dropout=-0.5)
-    with pytest.raises(ValueError):
-        model = create_model(dropout=1.2)
+        create_model(dropout=dropout)
