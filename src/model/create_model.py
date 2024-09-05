bc9cbba8e8c3b33ad8ddc94336e45b97d0e73b70
MBenediktF <mbf@fassian.com>
2024-09-05 11:02:58 +0200

    refactor: added input layer (best practive)

--- src/model/create_model.py
+++ src/model/create_model.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 def create_model(dropout=0.2):
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.InputLayer(shape=(28, 28)),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(dropout),
         tf.keras.layers.Dense(10)
