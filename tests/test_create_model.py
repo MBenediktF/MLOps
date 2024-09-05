beeb73ad52de142fdb81a64047b39de07dae1143
MBenediktF <mbf@fassian.com>
2024-09-05 14:27:12 +0200

    dev: comments and rfaise test

--- tests/test_create_model.py
+++ tests/test_create_model.py
@@ -1,8 +1,16 @@
 from src.model.create_model import create_model
 import tensorflow as tf
+import pytest
 
 
 def test_create_model():
+    # check if the function returns a valid Keras model
     model = create_model(dropout=0.2)
     assert isinstance(model, tf.keras.Model), "The returned model is not a Keras model"
     assert len(model.layers) > 0, "The returned model has no layers"
+
+    # check how the funtion handles wrong dropout parameter
+    with pytest.raises(ValueError):
+        model = create_model(dropout=-0.5)
+    with pytest.raises(ValueError):
+        model = create_model(dropout=1.2)
