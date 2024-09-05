ce23f7d917959d493ac3ecaa945f2950dc01cf13
MBenediktF <mbf@fassian.com>
2024-09-05 15:05:02 +0200

    feat: input verification

--- src/model/preprocess_data.py
+++ src/model/preprocess_data.py
@@ -1,5 +1,9 @@
 def preprocess_data(x_train, x_test):
-    # Normalisierung der Daten
+    # Check input data
+    if x_train < 0 or x_train > 255 or x_test < 0 or x_test > 255:
+        raise ValueError("Input data must be between 0 and 255")
+
+    # Normalize data
     x_train = x_train / 255.0
     x_test = x_test / 255.0
     return x_train, x_test
