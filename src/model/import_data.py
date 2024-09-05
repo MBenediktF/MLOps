46107a20bef1574ec5d8afb7446f507c051944a6
MBenediktF <mbf@fassian.com>
2024-09-05 15:23:57 +0200

    refactor: remofec subfunction

--- src/model/import_data.py
+++ src/model/import_data.py
@@ -2,10 +2,14 @@ import pandas as pd
 import os
 
 
-def _load_mnist_from_csv(train_csv, test_csv):
+def import_data():
+    project_root = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', '..'))
+    train_csv_path = os.path.join(project_root, 'datasets', 'mnist_train.csv')
+    test_csv_path = os.path.join(project_root, 'datasets', 'mnist_test.csv')
 
-    train_df = pd.read_csv(train_csv)
-    test_df = pd.read_csv(test_csv)
+    train_df = pd.read_csv(train_csv_path)
+    test_df = pd.read_csv(test_csv_path)
 
     print(train_df.head())
 
@@ -18,15 +22,4 @@ def _load_mnist_from_csv(train_csv, test_csv):
     x_train = x_train.reshape(-1, 28, 28)
     x_test = x_test.reshape(-1, 28, 28)
 
-    return x_train, y_train, x_test, y_test
-
-
-def import_data():
-    project_root = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..', '..'))
-    train_csv_path = os.path.join(project_root, 'datasets', 'mnist_train.csv')
-    test_csv_path = os.path.join(project_root, 'datasets', 'mnist_test.csv')
-    x_train, y_train, x_test, y_test = _load_mnist_from_csv(
-        train_csv_path,
-        test_csv_path)
     return x_train, y_train, x_test, y_test
