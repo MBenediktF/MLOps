ed5025ae5d6186f231cc219b04e4fc2360eea8e7
MBenediktF <mbf@fassian.com>
2024-09-06 10:29:27 +0200

    fix: missing epoch variable in function call

--- src/model/fit_model.py
+++ src/model/fit_model.py
@@ -3,6 +3,6 @@ def fit_model(model, x_train, y_train, optimizer, loss, metrics, epochs):
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
     # Fit model
-    history = model.fit(x_train, y_train, epochs=5)
+    history = model.fit(x_train, y_train, epochs=epochs)
 
     return history
