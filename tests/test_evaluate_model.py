048be2646908a667a563670e78ac82921a7b04fc
MBenediktF <mbf@fassian.com>
2024-09-06 10:46:40 +0200

    dev: added valid test

---
+++ tests/test_evaluate_model.py
@@ -0,0 +1,22 @@
+from src.model.evaluate_model import evaluate_model
+import tensorflow as tf
+
+
+model = tf.keras.Sequential([
+        tf.keras.layers.InputLayer(shape=(4, 4)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
+    ])
+x_test = tf.random.normal((10, 4, 4))
+y_test = tf.random.uniform((10,), maxval=10, dtype=tf.int32)
+
+
+def test_evaluate_model_valid():
+    model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+    test_loss, test_acc = evaluate_model(model, x_test, y_test)
+    assert isinstance(test_loss, float), \
+        "The returned loss is not a float"
+    assert isinstance(test_acc, float), \
+        "The returned accuracy is not a float"
