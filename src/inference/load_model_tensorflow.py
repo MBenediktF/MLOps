5b67364e06f52c829a36e2d8d2bbecb933b28c4d
MBenediktF <mbf@fassian.com>
2024-09-30 10:25:52 +0200

    feat: added exception

--- src/inference/load_model_tensorflow.py
+++ src/inference/load_model_tensorflow.py
@@ -3,17 +3,25 @@ import mlflow
 
 def load_model(experiment: str, run_id: str):
     mlflow.set_experiment(experiment)
-    model = mlflow.tensorflow.load_model(f"runs:/{run_id}/model")
+
+    try:
+        model = mlflow.tensorflow.load_model(f"runs:/{run_id}/model")
+    except Exception:
+        raise Exception("Model could not be loaded")
 
     return model
 
 
 def load_model_with_best_accuracy(experiment: str):
     mlflow.set_experiment(experiment)
-    runs = mlflow.search_runs(experiment,
-                              order_by=["metrics.accuracy DESC"],
-                              max_results=1)
-    best_run_id = runs[0].info.run_id
-    model = mlflow.tensorflow.load_model(f"runs:/{best_run_id}/model")
+
+    try:
+        runs = mlflow.search_runs(
+            experiment, order_by=["metrics.accuracy DESC"], max_results=1
+        )
+        best_run_id = runs[0].info.run_id
+        model = mlflow.tensorflow.load_model(f"runs:/{best_run_id}/model")
+    except Exception:
+        raise Exception("Model could not be loaded")
 
     return model
