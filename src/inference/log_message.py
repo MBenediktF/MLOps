4d2f5b326d8460e745c5a576ec3a4cbe33a1bf00
MBenediktF <mbf@fassian.com>
2024-09-30 09:40:08 +0200

    dev: logging levels export & default

--- src/inference/log_message.py
+++ src/inference/log_message.py
@@ -1,11 +1,17 @@
 import logging
 
 
+INFO = logging.INFO
+WARNING = logging.WARNING
+ERROR = logging.ERROR
+
+
 logging.basicConfig(
     filename='inference_pipeline.log',
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=INFO
 )
 
 
-def log_message(message: str, level: int = logging.INFO):
+def log_message(message: str, level: int = INFO):
     logging.log(level, message)
