176aa87d25e9c2af69bc839b62d4deb4f132ae74
MBenediktF <mbf@fassian.com>
2024-10-03 15:33:41 +0200

    dev: image capturing and api call

---
+++ src/edge/capture_image.py
@@ -0,0 +1,23 @@
+import libcamera
+from picamera2 import Picamera2
+import time
+import requests
+import cv2
+
+picam = Picamera2()
+config = picam.create_preview_configuration(main={"size": (640, 480)})
+config["transform"] = libcamera.Transform(hflip=0, vflip=1)
+picam.configure(config)
+picam.start()
+
+api_url = "192.168.178.133:5001/predict"
+
+while True:
+	image = picam.capture_array()
+	_, encoded_image = cv2.imencode('.jpg', image)
+	files = {'file': ('image.jpg', encoded_image.tobytes(), 'image/jpeg')}
+	response = requests.post(api_url, files=files)
+	print(f"API Response: {response.status_code}, {response.text}")
+	time.sleep(1)
+	
+picam.close()
