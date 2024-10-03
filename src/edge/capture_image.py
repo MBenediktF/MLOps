e080fa6405981099de6f0e0ea0d0503cbffdf57c
MBenediktF <mbf@fassian.com>
2024-10-03 16:32:01 +0200

    dev: url and file name changes

--- src/edge/capture_image.py
+++ src/edge/capture_image.py
@@ -10,12 +10,12 @@ config["transform"] = libcamera.Transform(hflip=0, vflip=1)
 picam.configure(config)
 picam.start()
 
-api_url = "192.168.178.133:5001/predict"
+api_url = "http://192.168.178.133:5001/upload"
 
 while True:
 	image = picam.capture_array()
 	_, encoded_image = cv2.imencode('.jpg', image)
-	files = {'file': ('image.jpg', encoded_image.tobytes(), 'image/jpeg')}
+	files = {'image': ('image.jpg', encoded_image.tobytes(), 'image/jpeg')}
 	response = requests.post(api_url, files=files)
 	print(f"API Response: {response.status_code}, {response.text}")
 	time.sleep(1)
