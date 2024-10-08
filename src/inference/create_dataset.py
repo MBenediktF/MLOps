8baabc9ae855e6970d77ce2903e5b7e7cae5d0ea
MBenediktF <mbf@fassian.com>
2024-10-08 10:38:35 +0200

    init: main structure

---
+++ src/inference/create_dataset.py
@@ -0,0 +1,11 @@
+# Read the data from influx and s3 in order to create a new dataset
+# with features and labels fron an measuerment
+
+def create_dataset(measurement):
+    # fetch datapoints from influx
+
+    # read images from s3
+
+    # create iage metadata string
+
+    # save images
