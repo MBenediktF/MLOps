3cbb5a7cd0c95a77fce3d5cf1f86b80873981d59
MBenediktF <mbf@fassian.com>
2024-10-10 13:19:56 +0200

    feat: modularization

--- src/model/import_dataset.py
+++ src/model/import_dataset.py
@@ -3,28 +3,29 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 
-IMAGE_WIDTH = 100
-IMAGE_HEIGHT = 75
+# @TODO get this from the dataset
+width = 100
+height = 75
 
+# @TODO get this from environment
 S3_ENDPOINT = "http://localhost:9000"
 enable_local_dev(S3_ENDPOINT)
 
-dataset_uuid = "ea93eb20-616c-4ad2-9d82-cca701766612"
 
-dataset = download_dataset(dataset_uuid)
-if not dataset:
-    raise Exception("Could not download dataset")
+def import_dataset(dataset_uuid):
+    dataset = download_dataset(dataset_uuid)
+    if not dataset:
+        raise Exception("Could not download dataset")
 
+    images = np.zeros((len(dataset), height, width, 3), dtype=np.uint8)
+    labels = np.zeros((len(dataset), 1), dtype=np.uint8)
 
-images = np.zeros((len(dataset), IMAGE_HEIGHT, IMAGE_WIDTH, 3), dtype=np.uint8)
-labels = np.zeros((len(dataset), 1), dtype=np.uint8)
+    for i, (filename, image) in enumerate(dataset.items()):
+        try:
+            images[i] = np.array(Image.open(BytesIO(image)))
+        except Exception as e:
+            print(f"Could not read image: {e}")
+            continue
+        labels[i] = int(filename.split('_')[-1].split('.')[0])
 
-for i, (filename, image) in enumerate(dataset.items()):
-    try:
-        images[i] = np.array(Image.open(BytesIO(image)))
-    except Exception as e:
-        print(f"Could not read image: {e}")
-        continue
-    labels[i] = int(filename.split('_')[-1].split('.')[0])
-
-print(labels)
+    return images, labels, width, height
