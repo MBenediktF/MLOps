3e169071bc0bd5968a75b63b7b0498a190d4f369
MBenediktF <mbf@fassian.com>
2024-10-11 09:19:13 +0200

    dev: added uids

--- src/model/import_dataset.py
+++ src/model/import_dataset.py
@@ -4,8 +4,8 @@ from io import BytesIO
 from PIL import Image
 
 # @TODO get this from the dataset
-width = 100
-height = 75
+img_width = 100
+img_height = 75
 
 # @TODO get this from environment
 S3_ENDPOINT = "http://localhost:9000"
@@ -17,9 +17,10 @@ def import_dataset(dataset_uuid):
     if not dataset:
         raise Exception("Could not download dataset")
 
-    images = np.zeros((len(dataset), height, width, 3), dtype=np.uint8)
-    labels = np.zeros((len(dataset), 1), dtype=np.uint8)
-
+    # restructure dataset
+    images = np.zeros((len(dataset), img_height, img_width, 3), dtype=np.uint8)
+    labels = np.zeros((len(dataset)), dtype=np.uint8)
+    uids = np.zeros((len(dataset)), dtype='U36')
     for i, (filename, image) in enumerate(dataset.items()):
         try:
             images[i] = np.array(Image.open(BytesIO(image)))
@@ -27,5 +28,6 @@ def import_dataset(dataset_uuid):
             print(f"Could not read image: {e}")
             continue
         labels[i] = int(filename.split('_')[-1].split('.')[0])
+        uids[i] = filename.split('/')[2].split('_')[0]
 
-    return images, labels, width, height
+    return images, labels, uids, img_width, img_height
