9046c0cf5be25b54cd552575c5bca55fca265b4c
MBenediktF <mbf@fassian.com>
2024-10-11 09:38:45 +0200

    dev: dynamic width & height

--- src/model/import_dataset.py
+++ src/model/import_dataset.py
@@ -3,10 +3,6 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 
-# @TODO get this from the dataset
-img_width = 100
-img_height = 75
-
 # @TODO get this from environment
 S3_ENDPOINT = "http://localhost:9000"
 enable_local_dev(S3_ENDPOINT)
@@ -16,6 +12,10 @@ def import_dataset(dataset_uuid):
     dataset = download_dataset(dataset_uuid)
     if not dataset:
         raise Exception("Could not download dataset")
+
+    # get image width and height
+    first_image = next(iter(dataset.values()))
+    img_width, img_height = Image.open(BytesIO(first_image)).size
 
     # restructure dataset
     images = np.zeros((len(dataset), img_height, img_width, 3), dtype=np.uint8)
@@ -30,4 +30,4 @@ def import_dataset(dataset_uuid):
         labels[i] = int(filename.split('_')[-1].split('.')[0])
         uids[i] = filename.split('/')[2].split('_')[0]
 
-    return images, labels, uids, img_width, img_height
+    return images, labels, uids
