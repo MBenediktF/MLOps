c79a12884a462020f10e0c6f2c506896031a1b82
MBenediktF <mbf@fassian.com>
2024-10-14 09:41:51 +0200

    update: new mudules for new model type

--- src/experiment_single_run.py
+++ src/experiment_single_run.py
@@ -1,50 +1,63 @@
-import os
-import tensorflow as tf
-import certifi
+# import os
+# import tensorflow as tf
+# import certifi
 import mlflow
 import mlflow.tensorflow
 
-from model.import_data import import_data
+from model.import_dataset import import_dataset
 from model.preprocess_data import preprocess_data
 from model.create_model import create_model
 from model.fit_model import fit_model
 from model.evaluate_model import evaluate_model
 
-print("TensorFlow version:", tf.__version__)
+# print("TensorFlow version:", tf.__version__)
 
-os.environ['SSL_CERT_FILE'] = certifi.where()
+# os.environ['SSL_CERT_FILE'] = certifi.where()
 
-mlflow.set_tracking_uri("http://localhost:5003")
+
+mlflow_tracking_uri = "http://localhost:5003"
+dataset_id = "ea93eb20-616c-4ad2-9d82-cca701766612"
+experiment_name = "Experiment_1"
+test_split = 0.2
+dropout = 0.2
+epochs = 5
+
+mlflow.set_tracking_uri(mlflow_tracking_uri)
 
 
 def main():
     # Daten importieren
-    x_train, y_train, x_test, y_test = import_data()
+    images, labels, uids = import_dataset(dataset_id)
 
     # Daten vorverarbeiten
-    x_train, x_test = preprocess_data(x_train, x_test)
+    train_x, train_y, train_uids, test_x, test_y, test_uids = \
+        preprocess_data(images, labels, uids, test_split)
 
     # Experiment erstellen
-    mlflow.set_experiment("MNIST Single Run")
+    mlflow.set_experiment(experiment_name)
 
     with mlflow.start_run():
         mlflow.tensorflow.autolog()
 
         # Modell erstellen
-        model = create_model(dropout=0.2)
+        model = create_model(test_x.shape, dropout)
 
         # Modell trainieren
-        fit_model(model, x_train, y_train,
+        fit_model(model, train_x, train_y,
                   optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
+                  loss="mean_squared_error",
                   metrics=['accuracy'],
-                  epochs=5)
+                  epochs=epochs)
 
         # Modell evaluieren
-        eval = evaluate_model(model, x_test, y_test)
+        test_loss, test_acc = evaluate_model(model, test_x, test_y)
 
         # Metriken loggen
-        mlflow.tensorflow.mlflow.log_metric("evaluation_accuracy", eval[1])
+        mlflow.tensorflow.mlflow.log_metric("test_loss", test_loss)
+        mlflow.tensorflow.mlflow.log_metric("test_loss", test_acc)
+        mlflow.tensorflow.mlflow.log_param("dropout", dropout)
+        mlflow.tensorflow.mlflow.log_param("test_split", test_split)
+        mlflow.tensorflow.mlflow.log_param("dataset_id", dataset_id)
 
     mlflow.end_run()
 
