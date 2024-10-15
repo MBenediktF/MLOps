56fb5825f54736e1db44c8fba6f21ac62aff2d40
MBenediktF <mbf@fassian.com>
2024-10-15 19:18:06 +0200

    refactor: modularized code

--- src/edge/capture_image.py
+++ src/edge/capture_image.py
@@ -1,7 +1,5 @@
 import libcamera
 from picamera2 import Picamera2
-import time
-import requests
 import cv2
 
 picam = Picamera2()
@@ -10,14 +8,12 @@ config["transform"] = libcamera.Transform(hflip=0, vflip=1)
 picam.configure(config)
 picam.start()
 
-api_url = "http://192.168.178.133:5001/upload"
 
-while True:
-	image = picam.capture_array()
-	_, encoded_image = cv2.imencode('.jpg', image)
-	files = {'image': ('image.jpg', encoded_image.tobytes(), 'image/jpeg')}
-	response = requests.post(api_url, files=files)
-	print(f"API Response: {response.status_code}, {response.text}")
-	time.sleep(1)
-	
-picam.close()
+def capture_image_jpg():
+    try:
+        image = picam.capture_array()
+        _, encoded_image = cv2.imencode('.jpg', image)
+        file = ('image.jpg', encoded_image.tobytes(), 'image/jpeg')
+        return file
+    except Exception:
+        return False
