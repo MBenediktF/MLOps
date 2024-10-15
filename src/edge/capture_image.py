d1b2e206811583918ce79c9ed68f692b7748fc6a
MBenediktF <mbf@fassian.com>
2024-10-15 19:42:14 +0200

    feat: increased image quality

--- src/edge/capture_image.py
+++ src/edge/capture_image.py
@@ -3,7 +3,7 @@ from picamera2 import Picamera2
 import cv2
 
 picam = Picamera2()
-config = picam.create_preview_configuration(main={"size": (640, 480)})
+config = picam.create_preview_configuration(main={"size": (2592, 1944)})
 config["transform"] = libcamera.Transform(hflip=0, vflip=1)
 picam.configure(config)
 picam.start()
@@ -12,6 +12,7 @@ picam.start()
 def capture_image_jpg():
     try:
         image = picam.capture_array()
+        image = cv2.resize(image, (640, 480))
         _, encoded_image = cv2.imencode('.jpg', image)
         file = ('image.jpg', encoded_image.tobytes(), 'image/jpeg')
         return file
