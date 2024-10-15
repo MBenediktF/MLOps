baa1f12251ae12d4917783dd9a6ec68b1e1ec12e
MBenediktF <mbf@fassian.com>
2024-10-15 20:21:41 +0200

    dev: camera close at exit

--- src/edge/capture_image.py
+++ src/edge/capture_image.py
@@ -1,12 +1,15 @@
 import libcamera
 from picamera2 import Picamera2
 import cv2
+import atexit
 
 picam = Picamera2()
 config = picam.create_preview_configuration(main={"size": (2592, 1944)})
 config["transform"] = libcamera.Transform(hflip=0, vflip=1)
 picam.configure(config)
 picam.start()
+
+atexit.register(picam.close)
 
 
 def capture_image_jpg():
