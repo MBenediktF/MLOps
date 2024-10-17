143e76a8d5aded31bf8b8ad32461911dba30d4f3
MBenediktF <mbf@fassian.com>
2024-10-17 09:45:32 +0200

    feat: led_output

--- src/edge/collect_data.py
+++ src/edge/collect_data.py
@@ -3,6 +3,7 @@ from api_helpers import send_measurement_to_api
 from capture_image import capture_image_jpg
 from take_lidar_measurement import take_lidar_measurement
 from buzzer_output import set_beep_interval
+from led_output import set_led_output
 
 while True:
     time.sleep(0.25)
@@ -15,6 +16,7 @@ while True:
 
     # read lidar sensor
     sensor_value = take_lidar_measurement()
+    set_led_output(False)
     if sensor_value is None:
         print("Could not get measurement")
         continue
@@ -22,6 +24,7 @@ while True:
     if sensor_value > 250:
         print("Measurement out of range")
         continue
+    set_led_output(True)
 
     # api call
     send_measurement_to_api(image, sensor_value)
