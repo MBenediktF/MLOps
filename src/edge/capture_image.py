27514ace3e09218ca1f2db012f29c61e627d4e4a
MBenediktF <mbf@fassian.com>
2024-10-17 09:32:03 +0200

    dev: collor and trans correction

--- src/edge/capture_image.py
+++ src/edge/capture_image.py
@@ -5,7 +5,6 @@ import atexit
 
 picam = Picamera2()
 config = picam.create_preview_configuration(main={"size": (2592, 1944)})
-config["transform"] = libcamera.Transform(hflip=0, vflip=1)
 picam.configure(config)
 picam.start()
 
@@ -15,6 +14,7 @@ atexit.register(picam.close)
 def capture_image_jpg():
     try:
         image = picam.capture_array()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, (640, 480))
         _, encoded_image = cv2.imencode('.jpg', image)
         file = ('image.jpg', encoded_image.tobytes(), 'image/jpeg')
