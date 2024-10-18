365f5c56cf9684c3a9983c780458220443ba9570
MBenediktF <mbf@fassian.com>
2024-10-18 08:35:25 +0200

    refactor: linting

--- src/inference/log_message.py
+++ src/inference/log_message.py
@@ -1,23 +1,18 @@
 import logging
 import os
 
-
 INFO = logging.INFO
 WARNING = logging.WARNING
 ERROR = logging.ERROR
 
-
 filename = "logs/inference_pipeline.log"
 
-
 os.makedirs(os.path.dirname(filename), exist_ok=True)
-
 
 # Initialize logfile if not already existing
 if not os.path.exists(filename):
     with open(filename, mode='w') as file:
         file.write("")
-
 
 logging.basicConfig(
     filename=filename,
