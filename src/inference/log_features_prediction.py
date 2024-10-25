7bc6f8eccc3cfc3aa6510c4bcb7c5cf3efce6d54
MBenediktF <mbf@fassian.com>
2024-10-25 10:10:29 +0200

    dev: logging, bytefile upload

--- src/inference/log_features_prediction.py
+++ src/inference/log_features_prediction.py
@@ -1,12 +1,14 @@
 from uuid import uuid4
 from influx_helpers import write_record, create_record
-from s3_helpers import upload_file
+from s3_helpers import upload_image_from_bytefile
+from log_message import log_message, ERROR
 
 
 def log_features_prediction(
-        feature_file, prediction, sensor_value, measurement="m1"):
+        feature_file, prediction, sensor_value, measurement="unknown"):
     feature_file_name = save_image_to_s3(feature_file, measurement)
     if not feature_file_name:
+        log_message("Could not save image to S3", ERROR)
         raise ValueError("Invalid file type: Only .jpg files are allowed")
     write_inference_data_to_influx(
         feature_file_name, prediction, sensor_value, measurement)
@@ -16,6 +18,7 @@ def log_features_prediction(
 def write_inference_data_to_influx(
         image_url, prediction, sensor_value, measurement):
     if not isinstance(image_url, str):
+        log_message("image_url has to be a str", ERROR)
         raise ValueError(f"image_url has to be a str, got {type(image_url)}")
     record = create_record(measurement) \
         .field("feature_file_url", image_url) \
@@ -25,12 +28,10 @@ def write_inference_data_to_influx(
 
 
 def save_image_to_s3(image_file, measurement):
-    if not file_is_jpg(image_file):
-        return False
     filename = f'features/{measurement}/{uuid4()}.jpg'
-    upload_file(image_file, filename)
+    try:
+        upload_image_from_bytefile(image_file, filename)
+    except Exception as e:
+        log_message(f"Could not upload image to S3: {e}", ERROR)
+        return False
     return filename
-
-
-def file_is_jpg(file):
-    return file.filename.endswith(('.jpg', '.jpg'))
