e0c1b8674d676e1b1b529a1abda6c6115d7695b8
MBenediktF <mbf@fassian.com>
2024-11-25 17:28:42 +0100

    refactor: mlflow

--- src/dagster/definitions.py
+++ src/dagster/definitions.py
@@ -1,17 +1,13 @@
 from assets.dataset import dataset
 from assets.dataset_preprocessed import dataset_preprocessed
 from assets.model import model
-from assets.trained_model import trained_model
-from assets.train_data import train_data
-from assets.test_data import test_data
+from assets.experiment import experiment
 
 from dagster import Definitions
 
 defs = Definitions(assets=[
     dataset,
     dataset_preprocessed,
-    train_data,
-    test_data,
     model,
-    trained_model
+    experiment
     ])
