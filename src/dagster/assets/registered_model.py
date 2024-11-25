5ecd916ef933ed8a71e6de582022c4d7e7fa7c3c
MBenediktF <mbf@fassian.com>
2024-11-25 19:09:23 +0100

    feat: select and register model

---
+++ src/dagster/assets/registered_model.py
@@ -0,0 +1,62 @@
+from dagster import AssetExecutionContext, MetadataValue
+from dagster import asset, Config, MaterializeResult
+import os
+import json
+from dotenv import load_dotenv
+import mlflow
+from mlflow.tracking import MlflowClient
+
+load_dotenv()
+
+mlflow_port = os.getenv('MLFLOW_PORT')
+host = os.getenv('HOST')
+mlflow.set_tracking_uri(f"http://mlflow:{mlflow_port}")
+mlflow_url = f"{host}:{mlflow_port}/#"
+
+
+class RegisterConfig(Config):
+    auto: bool = True
+    model_name: str = ""
+    compare_metric: str = "test_mae"
+
+
+@asset(
+    deps=["experiment"],
+    group_name="Training",
+    kinds={"mlflow"},
+    description="Run MLFlow experiment"
+)
+def registered_model(
+    context: AssetExecutionContext,
+    config: RegisterConfig
+) -> MaterializeResult:
+    # get experiment and runs
+    with open("data/experiment.json", "r") as f:
+        experiment_metadata = json.load(f)
+    experiment_id = experiment_metadata["id"]
+    runs = mlflow.search_runs(experiment_id)
+
+    # select best run by metric
+    metric_str = f"metrics.{config.compare_metric}"
+    best_run = runs.loc[runs[metric_str].idxmax()]
+    best_run_id = best_run["run_id"]
+
+    # register model
+    if config.auto:
+        model_uri = f"runs:/{best_run_id}/model"
+        client = MlflowClient()
+        client.create_model_version(
+            name=config.model_name,
+            source=model_uri,
+            run_id=best_run_id,
+        )
+
+    run_url = f"{mlflow_url}/experiments/{experiment_id}/runs/{best_run_id}"
+    model_url = f"{mlflow_url}/models/{config.model_name}"
+    return MaterializeResult(
+        metadata={
+            "best_run": MetadataValue.text(best_run_id),
+            "run": MetadataValue.url(run_url),
+            "model": MetadataValue.url(model_url)
+        }
+    )
--- src/dagster/definitions.py
+++ src/dagster/definitions.py
@@ -2,6 +2,7 @@ from assets.dataset import dataset
 from assets.dataset_preprocessed import dataset_preprocessed
 from assets.model import model
 from assets.experiment import experiment
+from assets.registered_model import registered_model
 
 from dagster import Definitions
 
@@ -9,5 +10,6 @@ defs = Definitions(assets=[
     dataset,
     dataset_preprocessed,
     model,
-    experiment
+    experiment,
+    registered_model
     ])
