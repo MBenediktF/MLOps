72db9e1ca857fab25521fe23d047e29b503fde8e
MBenediktF <mbf@fassian.com>
2024-11-30 15:33:46 +0100

    dev: removed dagster volume

--- bash/restore_docker_volumes.sh
+++ bash/restore_docker_volumes.sh
@@ -4,4 +4,3 @@ docker run --rm -v sbmlops_research_minio_data:/data -v $(pwd)/backup:/backup bu
 docker run --rm -v sbmlops_research_mysql_data:/data -v $(pwd)/backup:/backup busybox tar xzf /backup/mysql_data_backup.tar.gz -C /data
 docker run --rm -v sbmlops_research_influxdb_data:/data -v $(pwd)/backup:/backup busybox tar xzf /backup/influxdb_data_backup.tar.gz -C /data
 docker run --rm -v sbmlops_research_influxdb_config:/data -v $(pwd)/backup:/backup busybox tar xzf /backup/influxdb_config_backup.tar.gz -C /data
-docker run --rm -v sbmlops_research_dagster_data:/data -v $(pwd)/backup:/backup busybox tar xzf /backup/dagster_data_backup.tar.gz -C /data
