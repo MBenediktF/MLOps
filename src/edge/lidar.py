fdbf27285450b658fb979d50e579d6d4c076f517
MBenediktF <mbf@fassian.com>
2024-12-17 14:04:56 +0100

    dev: removed timing budget

--- src/edge/lidar.py
+++ src/edge/lidar.py
@@ -3,7 +3,6 @@ import busio
 import adafruit_vl53l0x
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_vl53l0x.VL53L0X(i2c)
-sensor.measurement_timing_budget = 200000
 
 
 def take_lidar_measurement():
